af31ff5d5ac8a0a022744fca072b9fbe727dbbca
renato <renatodiniz25@gmail.com>
2022-11-13 14:40:46 -0300

    Show essays on homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, doc, deleteDoc } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 function Home() {
+  const [essays, setEssays] = useState([]);
+  const essaysCollectionRef = collection(db, "essays");
+
+  useEffect(() => {
+    fetchEssays();
+  }, []);
+
+  async function fetchEssays() {
+    const data = await getDocs(essaysCollectionRef);
+    setEssays(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log(essays);
+  };
+
+  async function deleteEssay(id) {
+    const essayDoc = doc(db, "essays", id);
+    await deleteDoc(essayDoc);
+  };
+
   return (
-    <h2>Home Page</h2>
+    <>
+      <h2>Home Page</h2>
+      <div>
+        {essays.map((essay) => {
+          return (
+          <div>
+            <h3>{essay.title}</h3>
+            <p>{essay.body}</p>
+            <p>{essay.author.name}</p>
+            <button onClick={() => deleteEssay(essay.id)}>&#128465;</button>
+          </div>
+        )})}
+      </div>
+    </>
   );
 };
 
